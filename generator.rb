1da5c6d6da3e09a0c509fa6f0f1133557a67027e
ka-ni-ke <erdikanik@gmail.com>
2019-12-13 21:39:29 +0300

    Add files via upload

---
+++ generator.rb
@@ -0,0 +1,27 @@
+require 'jwt'
+
+key_file = 'Keys file full name' #
+ecdsa_key = OpenSSL::PKey::EC.new IO.read key_file
+
+headers = {
+    'kid' => 'Key ID'
+}
+
+claims = {
+    'iss' => 'Your apple team id',
+    
+    # let's say that this two values are constants
+    'iat' => 1576252452,
+    'exp' => 1586256052,
+
+    'aud' => 'https://appleid.apple.com',
+    'sub' => 'your bundle identifier or Service ID',
+}
+
+token = JWT.encode claims, ecdsa_key, 'ES256', headers
+
+File.open('RUBI_output.txt', 'w') do |fo|
+    fo.puts token
+end
+
+puts 'done'
